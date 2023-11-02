5fba5123f529b02b69309e8d20f25d48bedeafcb
Jesse <jlarson@iworq.com>
2023-11-02 11:17:20 -0600

    http requests

---
+++ VUE-COURSE/forms/src/components/RatingControl.vue
@@ -0,0 +1,63 @@
+<template>
+  <ul>
+    <li><button :class="{active: modelValue === 'poor'}" type="button" @click="activate('poor')">Poor</button></li>
+    <li><button :class="{active: modelValue === 'average'}" type="button" @click="activate('average')">Average</button></li>
+    <li><button :class="{active: modelValue === 'great'}" type="button" @click="activate('great')">Great</button></li>
+  </ul>
+</template>
+
+<script>
+export default {
+    // data() {
+    //     return {
+    //         activeOption: this.modelValue,
+    //     }
+    // },
+    // computed: {
+    //     activeOption() {
+    //         return this.modelValue;
+    //     }
+    // },
+    methods: {
+        activate(option) {
+            // this.activeOption = option;
+            this.$emit('update:modelValue', option);
+        }
+    },
+    props: ['modelValue'],
+    emits: ['update:modelValue'],
+}
+</script>
+
+<style scoped>
+ul {
+    list-style: none;
+    margin: 0.5rem 0;
+    padding: 0;
+    display: flex;
+}
+
+li {
+    margin: 0 1rem;
+    border: 1px solid #ccc;
+    padding: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+button {
+    font: inherit;
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+}
+
+.active {
+    border-color: #a00078;
+}
+
+.active button {
+    color: #a00078;
+}
+</style>
